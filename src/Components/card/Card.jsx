7cd07e564c410dc6965443382f06539e67e944eb
Developmentexpert121 <developmentexpert121@gmail.com>
2022-10-27 19:22:55 +0530

    Social-Proof-section

--- src/Components/card/Card.jsx
+++ src/Components/card/Card.jsx
@@ -8,7 +8,6 @@ const Card = (props) => {
         <div className={`card border-0 position-relative ${classes.img_txt_mainn}`} style={{}}>
           <img src={props.images} className="w-100 rounded-0" alt="..." />
           <div className={` ${classes.text_imgg}`}>
-            {/* <h3>{props.HT} <span>{props.Total}</span></h3> */}
             <p className={`card-text ${classes.body_font14}`}>{props.title} <span><b>{props.title2}</b></span></p>
             
           </div>
